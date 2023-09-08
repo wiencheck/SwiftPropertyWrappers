ea2cc4dbb28b75c82a4b25b565f371fe24a29c16
Adam Wienconek <adam.wienconek@new-gravity.com>
2023-09-08 09:53:14 +0200

    Rename to SwiftPropertyWrappers

--- Package.swift
+++ Package.swift
@@ -4,15 +4,15 @@
 import PackageDescription
 
 let package = Package(
-    name: "StoragePropertyWrappers",
+    name: "SwiftPropertyWrappers",
     platforms: [
         .iOS(.v13)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "StoragePropertyWrappers",
-            targets: ["StoragePropertyWrappers"]),
+            name: "SwiftPropertyWrappers",
+            targets: ["SwiftPropertyWrappers"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -22,7 +22,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "StoragePropertyWrappers",
+            name: "SwiftPropertyWrappers",
             dependencies: ["FileHelper"]),
     ]
 )
