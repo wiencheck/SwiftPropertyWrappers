0ce1151a4a5a2e2da36c65bb65627daa4644df41
Adam Wienconek <adamwienconek@gmail.com>
2024-08-12 13:53:22 +0200

    Update keychain dependency

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "SwiftPropertyWrappers",
     platforms: [
         .iOS(.v13),
-        .macOS(.v10_15)
+        .macOS(.v11)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/wiencheck/FileHelper", branch: "master"),
-        .package(url: "https://github.com/auth0/SimpleKeychain", branch: "master")
+        .package(url: "https://github.com/auth0/SimpleKeychain", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
