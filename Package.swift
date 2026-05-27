e6a1114428364f29789d7d2c7befa548b902a3c1
Adam Wienconek <awienconek.sii@inpost.pl>
2026-05-27 11:20:07 +0200

    Added watchOS 11 support

--- Package.swift
+++ Package.swift
@@ -7,7 +7,8 @@ let package = Package(
     name: "SwiftPropertyWrappers",
     platforms: [
         .iOS(.v14),
-        .macOS(.v11)
+        .macOS(.v11),
+        .watchOS("11.0")
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
